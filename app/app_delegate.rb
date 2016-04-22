c9bf160f453dbe1c76813f91773b4e4f0b70d096
kayhide <kayhide@gmail.com>
2016-04-22 17:15:30 +0900

    move migration dir.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,5 +5,15 @@ class AppDelegate
 end
 
 if RUBYMOTION_ENV == 'development'
-  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'development.sqlite')
+  Dir[File.expand_path('resources/migrate/**/*.rb')].each { |f| require f }
+
+  database = 'development.sqlite'
+  # database = ':memory:'
+
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: database)
+  dir = File.join(NSBundle.mainBundle.resourcePath, 'migrate/books')
+
+  ActiveRecord::Migrator.migrate dir
 end
+
+
--- lib/motive_record.rb
+++ lib/motive_record.rb
@@ -6,7 +6,6 @@ require 'motive_model'
 require 'motion-securerandom'
 
 require 'motive_record/version'
-require 'motive_record/rake_tasks'
 require 'motive_record/hooks'
 require 'motive_record/ext'
 
