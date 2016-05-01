682945d1655413401641c28dda11864718d63621
kayhide <kayhide@gmail.com>
2016-05-01 11:57:30 +0900

    update app_delegate for Utils.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,16 +4,42 @@ class AppDelegate
   end
 end
 
-if RUBYMOTION_ENV == 'development'
-  Dir[File.expand_path('resources/migrate/**/*.rb')].each { |f| require f }
+Dir[File.expand_path('resources/migrate/**/*.rb')].each { |f| require f }
 
-  database = 'development.sqlite'
-  # database = ':memory:'
+if RUBYMOTION_ENV != 'test'
+  module Utils
+    module_function
 
-  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: database)
-  dir = File.join(NSBundle.mainBundle.resourcePath, 'migrate/books')
+    def database
+      @database ||= "#{RUBYMOTION_ENV}.sqlite3"
+    end
 
-  ActiveRecord::Migrator.migrate dir
+    def database= arg
+      @database = arg
+    end
+
+    def db_use_memory
+      @database = ':memory:'
+      ActiveRecord::Base.clear_all_connections!
+      db_migrate
+    end
+
+    def db_drop
+      ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: database)
+      if ActiveRecord::Migrator.get_all_versions.any?
+        database = ActiveRecord::Base.connection_config[:database]
+        mp database
+        ActiveRecord::Base.clear_all_connections!
+        File.delete database if File.exist? database
+      end
+    end
+
+    def db_migrate
+      ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: database)
+      dir = File.join(NSBundle.mainBundle.resourcePath, 'migrate/books')
+      ActiveRecord::Migrator.migrate dir
+    end
+  end
+
+  Utils.db_migrate
 end
-
-
